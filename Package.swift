c1c4f1107738e4908e6597177cb1243c2c184041
Guilherme Souza <ogrsouza@gmail.com>
2020-08-05 21:46:22 -0300

    chore: set platform and swift language version on Package.swift (#5)

--- Package.swift
+++ Package.swift
@@ -4,25 +4,26 @@
 import PackageDescription
 
 let package = Package(
-    name: "Box",
-    products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "Box",
-            targets: ["Box"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "Box",
-            dependencies: []),
-        .testTarget(
-            name: "BoxTests",
-            dependencies: ["Box"]),
-    ]
+  name: "Box",
+  platforms: [
+    .iOS(.v8),
+    .macOS(.v10_10),
+    .tvOS(.v9),
+    .watchOS(.v2)
+  ],
+  products: [
+    .library(
+      name: "Box",
+      targets: ["Box"]),
+  ],
+  dependencies: [],
+  targets: [
+    .target(
+      name: "Box",
+      dependencies: []),
+    .testTarget(
+      name: "BoxTests",
+      dependencies: ["Box"]),
+  ],
+  swiftLanguageVersions: [.v5]
 )
--- Sources/Box/Box.swift
+++ Sources/Box/Box.swift
@@ -2,34 +2,34 @@
 @propertyWrapper
 @dynamicMemberLookup
 public final class Box<Wrapped> {
-
-    public internal(set) var wrappedValue: Wrapped
-
-    public var projectedValue: Box<Wrapped> { self }
-
-    public init(wrappedValue value: Wrapped) {
-        self.wrappedValue = value
-    }
-
-    public subscript<Value>(dynamicMember keyPath: KeyPath<Wrapped, Value>) -> Value {
-        wrappedValue[keyPath: keyPath]
-    }
+  
+  public internal(set) var wrappedValue: Wrapped
+  
+  public var projectedValue: Box<Wrapped> { self }
+  
+  public init(wrappedValue value: Wrapped) {
+    self.wrappedValue = value
+  }
+  
+  public subscript<Value>(dynamicMember keyPath: KeyPath<Wrapped, Value>) -> Value {
+    wrappedValue[keyPath: keyPath]
+  }
 }
 
 extension Box: Equatable where Wrapped: Equatable {
-    public static func == (lhs: Box<Wrapped>, rhs: Box<Wrapped>) -> Bool {
-        lhs.wrappedValue == rhs.wrappedValue
-    }
+  public static func == (lhs: Box<Wrapped>, rhs: Box<Wrapped>) -> Bool {
+    lhs.wrappedValue == rhs.wrappedValue
+  }
 }
 
 extension Box: Hashable where Wrapped: Hashable {
-    public func hash(into hasher: inout Hasher) {
-        wrappedValue.hash(into: &hasher)
-    }
+  public func hash(into hasher: inout Hasher) {
+    wrappedValue.hash(into: &hasher)
+  }
 }
 
 extension Box: Comparable where Wrapped: Comparable {
-    public static func < (lhs: Box<Wrapped>, rhs: Box<Wrapped>) -> Bool {
-        lhs.wrappedValue < rhs.wrappedValue
-    }
+  public static func < (lhs: Box<Wrapped>, rhs: Box<Wrapped>) -> Bool {
+    lhs.wrappedValue < rhs.wrappedValue
+  }
 }
